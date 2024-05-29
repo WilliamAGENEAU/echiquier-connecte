fe9d7c93d180567c8e62a43fdf1b7a820b5acb85
WilliamAGENEAU <wageneau.ir2024@esaip.org>
2024-05-29 13:34:41 +0200

    readme commit

--- lib/bloc/app_blocs.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app_echiquier/bloc/cubits/game_cubit.dart';
-import 'package:app_echiquier/bloc/cubits/settings_cubit.dart';
-import 'package:get_it/get_it.dart';
-
-createAppBlocs() {
-  GetIt.I.registerSingleton<GameCubit>(GameCubit.initial());
-  GetIt.I.registerSingleton<SettingsCubit>(SettingsCubit.initial());
-}
